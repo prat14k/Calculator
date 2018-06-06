2fda799f19e27de453ae0ecd029bc6d48d097ef5
Prateek Sharma <prateek13103718@gmail.com>
2018-06-07 00:44:07 +0530

    Project initiation, Defined UI created, Backend remaining
    - Project created, all starting files created
    - Button arrainged as per the document and size classes used to reshape them
    - TextField added to the ui
    - UI designed as per iphone landscape and protrait

---
+++ Calculator/CalculatorViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CalculatorViewController.swift
+//  Calculator
+//
+//  Created by Prateek Sharma on 6/6/18.
+//  Copyright © 2018 Prateek Sharma. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    override var preferredStatusBarStyle: UIStatusBarStyle { return .lightContent }
+
+    
+    
+}
+
